a007a7c51d22c01b2f6f0489dbef22774a12825d
kadirtikil <kadirtikil@gmail.com>
2024-06-24 21:53:22 +0200

    lists data correctly.

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,21 +1,46 @@
 import './Body.css';
 
+
 import Post from '../post/Post';
+import React, { useState, useEffect } from 'react';
 
+interface Post {
+    id: number;
+    title: string;
+    description: string;
+    tags: string;
+    author: string;
+    // Add other fields as necessary
+}
 
-export default function Body(){
+const MyComponent: React.FC = () => {
+    const [posts, setPosts] = useState<Post[]>([]);
 
-    return(
+    function fetchPostsFromAPI() {
+        fetch("http://localhost:8000/api/fetchposts")
+            .then((response) => response.json())
+            .then((data) => {
+                setPosts(data.ok);
+            });
+    }
+
+    useEffect(() => {
+        fetchPostsFromAPI();
+    }, []);
+
+    return (
         <>
             <div className="rootBody">
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                {posts.map((post) => (
+                    <div className='postRootBody' key={post.id}>
+                        <h3>{post.title}</h3>
+                        <h6><strong>Author:</strong> {post.author}</h6>
+                    </div>
+                ))}
             </div>
         </>
-    )
-}
+    );
+};
+
+export default MyComponent;
+
